34a29f668ed2cdb839b6cfd1503198b40de957fa
Will 保哥 <doggy.huang@gmail.com>
2021-08-21 10:53:49 +0800

    示範預設路由、轉向路由與萬用路由用法

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Page1Component } from './page1/page1.component';
 import { NgModule } from '@angular/core';
@@ -5,9 +6,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
