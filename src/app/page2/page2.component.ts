8e59832aa001cea77ec2bdff6ca88440d96933d0
Will 保哥 <doggy.huang@gmail.com>
2021-08-21 10:06:12 +0800

    加入 page1, page2, dashboard 元件並設定路由定義與調整 <router-outlet> 位置與啟用 HashLocationStrategy

---
+++ src/app/page2/page2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './page2.component.html',
+  styleUrls: ['./page2.component.css']
+})
+export class Page2Component {
+
+  constructor() { }
+
+}
