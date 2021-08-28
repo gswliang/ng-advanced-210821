7dfd9bb0995c91444367a661c1b9e876926315fd
Will 保哥 <doggy.huang@gmail.com>
2021-08-28 13:31:57 +0800

    先將 login 元件完整複製到 login2 元件

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { NgForm, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './login2.component.html',
@@ -6,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Login2Component implements OnInit {
 
-  constructor() { }
+  origBodyClassName: string;
+
+  data: any = {
+    email: '',
+    mima: '1231123123',
+    isRememberMe: true
+  };
+
+  constructor(@Inject(DOCUMENT) private document: Document,
+    private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.origBodyClassName = this.document.body.className;
+    this.document.body.className = 'bg-gradient-primary';
   }
 
+  doLogin(form: NgForm) {
+
+    if (form.valid) {
+        // 假設登入驗證成功，寫入 Token 到 localStorage 中
+        localStorage.setItem('token', '123');
+        var url = this.route.snapshot.queryParamMap.get('returnUrl');
+        if (!!url) {
+          this.router.navigateByUrl(url);
+        } else {
+          this.router.navigateByUrl('/');
+        }
+    }
+
+    if (form.invalid) {
+      let errMsg = '';
+      for (const fieldName of Object.keys(form.controls)) {
+        let ctrl = form.controls[fieldName] as FormControl;
+        if (ctrl.invalid) {
+          let fieldValue = ctrl.value;
+          errMsg += `欄位 ${fieldName} 發生錯誤: ${fieldValue}\r\n`;
+        }
+      }
+      alert('表單驗證失敗，請正確填寫後再送出一次！\r\n' + errMsg);
+    }
+
+  }
+
+  ngOnDestroy(): void {
+    this.document.body.className = this.origBodyClassName;
+  }
 }
